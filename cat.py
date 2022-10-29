7a6aaa37a9f1c948d985f045f2ac972f36126ed0
MimicTester1307 <excel.chukwu@ashesi.edu.gh>
2022-10-29 13:28:13 +0000

    create parser and add arguments
    Signed-off-by: MimicTester1307 <excel.chukwu@ashesi.edu.gh>

--- cat.py
+++ cat.py
@@ -1,1 +1,12 @@
 import argparse
+
+parser = argparse.ArgumentParser(
+    prog="cat",
+    description="a simple python implementation of the UNIX cat command",
+    epilog="built by GDSC Ashesi. \
+            You are welcome to modify and reuse this code as needed"
+)
+
+parser.add_argument('filename', help="the file to be displayed")  # use this for test-allow multiple files to be read, you'll need to know about Python's kwargs and args
+parser.add_argument('-n', '--number', help="the number of bytes to read from the file", type=int)
+parser.add_argument('-l', '--lines', help="the number of lines you want to read from the file.", type=int)
