c0eac6253cf8fe3da9099888a2c44f671dba06f6
MimicTester1307 <excel.chukwu@ashesi.edu.gh>
2022-10-29 13:36:20 +0000

    create logic for processing arguments
    Signed-off-by: MimicTester1307 <excel.chukwu@ashesi.edu.gh>

--- cat.py
+++ cat.py
@@ -10,3 +10,21 @@ parser = argparse.ArgumentParser(
 parser.add_argument('filename', help="the file to be displayed")  # use this for test-allow multiple files to be read, you'll need to know about Python's kwargs and args
 parser.add_argument('-n', '--number', help="the number of bytes to read from the file", type=int)
 parser.add_argument('-l', '--lines', help="the number of lines you want to read from the file.", type=int)
+
+
+# parsing the created arguments for use:
+args = parser.parse_args()
+
+
+if args.number:
+    with open(args.filename, 'r') as fp:
+        print(fp.read(args.number))  # if type is not specified in argument, you'll have to convert the number to an int first before using it in read
+elif args.lines:
+    # we can also do fp.readlines(args.lines), but it returns a list, and we will 
+    # still need to loop
+    with open(args.filename, 'r') as fp:
+        for i in range(args.lines):
+            print(fp.readline())
+else:
+    with open(args.filename, 'r') as fp:
+        print(fp.read())
